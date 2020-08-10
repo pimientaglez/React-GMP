23a9b17015282dd56a00039952965a2bca29624e
Ricardo Pimienta <pimientaglez@gmail.com>
2020-08-10 00:54:39 -0500

    ResultCount component

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import Menu from "../Menu/Menu";
 import './Main.scss';
+import ResultCount from '../ResultCount/ResultCount';
 
 class Main extends Component {
     render() {
         return (
             <div className="main">
                 <Menu />
+                <ResultCount />
             </div>
         );
     }
