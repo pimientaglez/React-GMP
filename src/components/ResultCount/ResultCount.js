e579ba8a0f4939f56020bd10141ab41eaaae34ec
Ricardo Pimienta <pimientaglez@gmail.com>
2020-08-10 00:55:14 -0500

    ResultCount Component created

---
+++ src/components/ResultCount/ResultCount.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './ResultCount.scss';
+
+class ResultCount extends Component {
+    render() {
+        return (
+            <div className="result-count">
+                <h3>39</h3><p>movies found</p>
+            </div>
+        );
+    }
+}
+
+export default ResultCount;
