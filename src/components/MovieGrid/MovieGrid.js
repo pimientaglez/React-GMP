769f45b21158ad67287b2d6b3d461a1a1462e463
Ricardo Pimienta <pimientaglez@gmail.com>
2020-08-10 00:03:56 -0500

    MovieGrid and MovieCard components

---
+++ src/components/MovieGrid/MovieGrid.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './MovieGrid.scss';
+
+class MovieGrid extends Component {
+    render() {
+        return (
+            <div className="movie-grid">
+                
+            </div>
+        );
+    }
+}
+
+export default MovieGrid;
